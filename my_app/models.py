ab1c2c7f02c125b59d5efb575c28512dc39aba50
AndrewZacharyLiu <87785681+AndrewZacharyLiu@users.noreply.github.com>
2024-02-12 21:57:40 -0500

    MODELS.PY

--- my_app/models.py
+++ my_app/models.py
@@ -7,3 +7,29 @@ class Student(models.Model):
     grade = models.CharField(max_length=255)
     gpa = models.FloatField()
 
+class User(models.Model):
+    email = models.EmailField()
+    username = models.CharField(max_length=255, primary_key=True)
+    password = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+
+class Group(models.Model):
+    groupID = models.AutoField(primary_key=True)
+    groupgoal_id = models.CharField(max_length=255)
+
+class UserJoinGroup(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    category_id = models.CharField(max_length=255, primary_key=True)
+
+class Transactions(models.Model):
+    week = models.DateField()
+    amount = models.FloatField()
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class UserJoinCategory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
