136e42428e5d5df6a970a647b2efb871c5f3c077
sduprey <sam.d.duprey@gmail.com>
2024-02-17 13:23:42 -0500

    added sign up page functionality

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,31 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+# class SignUpView(CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy("login")
+#     template_name = "registration/signup.html"
+
+from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+from django.shortcuts import render, redirect
+
+def home_view(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.user.is_anonymous:
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        else:
+            form = SignUpForm()
+        return render(request, 'registration/signup.html', {'form': form})
+    else:
+        return render(request, 'home.html')
