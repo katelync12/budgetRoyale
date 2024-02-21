a1365e1be6c86524612ee2b2e3459091f0a1e760
katelync12 <katelynchen122132@gmail.com>
2024-02-20 20:17:37 -0500

    delete unnecessary import statement

--- my_app/models.py
+++ my_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django import forms
 from django.db import connection
-import pgtrigger
 
 class Student(models.Model):
     grade = models.CharField(max_length=255)
