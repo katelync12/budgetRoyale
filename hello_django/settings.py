afe42169a3b8a545e77cb660c0b499b8aeb622fc
AndrewZacharyLiu <87785681+AndrewZacharyLiu@users.noreply.github.com>
2024-02-26 10:06:40 -0500

    jjj

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
-
+#
 #EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 
